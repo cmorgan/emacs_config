5a206e7eb715c8399bbc347983b5ccc33b5cc474
Chris Morgan <chris.j.morgan@gmail.com>
2014-07-09 18:36:14 +0100

    cua mode

--- user.el
+++ user.el
@@ -6,6 +6,11 @@
 (desktop-save-mode 0)
 (global-auto-revert-mode t)  ; auto reload all buffers
 
+(cua-mode t)
+    (setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
+    (transient-mark-mode 1)               ;; No region when it is not highlighted
+    (setq cua-keep-region-after-copy t)
+
 (autoload 'markdown-mode "markdown-mode"
 		     "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
