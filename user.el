8c7687c1bbcdc2fd0af25b471f9055e5137ad36e
Chris Morgan <chris.j.morgan@gmail.com>
2014-07-12 22:42:26 +0100

    working

--- user.el
+++ user.el
@@ -65,6 +65,151 @@
 	 (define-key evil-visual-state-map (kbd ",c") 'evilnc-comment-or-uncomment-lines)
 	 ))
 
+(use-package paredit
+  :ensure t
+  :init (progn
+	  (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+	  (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+	  (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+	  (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+	  (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+	  (add-hook 'haskell-mode-hook          #'enable-paredit-mode)
+	  (add-hook 'inferior-lisp-mode-hook    #'enable-paredit-mode)
+	  (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+	  (add-hook 'clojure-mode-hook          #'enable-paredit-mode)
+	  (add-hook 'cider-repl-mode-hook       #'enable-paredit-mode)
+	  (add-hook 'scheme-mode-hook           #'enable-paredit-mode)))
+
+
+(use-package flycheck
+  :ensure t
+  :init (timeit
+	 "FLYCHECK"
+	 (add-hook 'after-init-hook #'global-flycheck-mode)
+	 (setq flycheck-highlighting-mode 'lines)
+	 (setq python-check-function "flake8")))
+
+
+(use-package magit
+  :ensure t
+  :config (progn
+	    (define-key evil-normal-state-map (kbd ",gb") 'magit-blame-mode)
+	    (global-set-key (kbd "C-x G") 'magit-status)))
+
+(use-package projectile
+  :ensure t
+  :init (timeit
+	 "PROJECTILE"
+	  (projectile-global-mode)
+	  (define-key evil-normal-state-map "\C-p" 'projectile-find-file)))
+
+
+(use-package ido
+  :ensure t
+  :init (timeit
+	 "IDO"
+	 (ido-mode t)
+	 (ido-everywhere t)
+	 (setq ido-enable-flex-matching t)
+	 (setq ido-file-extensions-order '(".py" ".js" ".emacs" t))))
+
+
+(use-package tool-bar
+  :init (progn
+	  (tool-bar-mode 0)))
+
+(use-package hl-line
+  :init (progn
+	  (global-hl-line-mode)))
+
+(use-package scroll-bar
+  :init (progn
+	  (scroll-bar-mode 0)))
+
+(use-package menu-bar
+  :init (progn
+	  (menu-bar-mode 0)))
+
+(use-package frame
+  :init (progn
+	  (blink-cursor-mode)))
+
+(use-package paren
+  :init (progn
+	  (show-paren-mode)))
+
+
+(use-package xclip
+  :ensure t
+  :init (timeit
+	 "XCLIP"
+	 (xclip-mode t)))
+
+
+(use-package text-mode
+  :mode "README"
+  :init (progn
+	  (add-hook 'text-mode-hook
+		    (lambda ()
+		      (flyspell-mode t)))))
+
+(use-package jedi
+  :ensure t
+  :commands jedi:setup
+  :config (timeit
+	   "JEDI"
+	   (setq jedi:complete-on-dot t)
+	   (setq jedi:tooltip-method nil)))
+
+(use-package help-mode
+  :init (progn
+	  (add-hook 'help-mode-hook
+		    (lambda ()
+		      (evil-motion-state 0)))))
+
+
+(use-package virtualenvwrapper
+  :ensure t
+  :defer t
+  :init (progn
+
+	  (defun venv-get-proj-dir ()
+	    "While in a venv, return the project directory or nil."
+	    (let ((fn (concat (getenv "VIRTUAL_ENV") ".project")))
+	      (if (file-exists-p fn)
+		  (chomp (read-file-in-string fn)))))
+
+	  (defun venv-cdproject ()
+	    "Change directory to the current project directory, if set."
+	    (interactive)
+	    (let ((dir (venv-get-proj-dir)))
+	      (if dir
+		  (cd dir))))
+
+	  (defun venv-workon-and-cdproject (venv)
+	    (interactive "i")
+	    (venv-workon venv)
+	    (venv-cdproject)
+	    (dired default-directory))
+
+	  (define-key evil-normal-state-map ",w" 'venv-workon-and-cdproject))
+
+  :config (timeit
+	   "VENV"
+	   (venv-initialize-interactive-shells)
+	   (venv-initialize-eshell)))
+
+
+(use-package js
+  :init (progn
+	  (add-hook 'js-mode-hook
+		    (lambda ()
+					(setq indent-tabs-mode nil
+                ;; evil-shift-width 2
+                ;; tab-width 2
+                js-indent-level 2)
+		      (modify-syntax-entry ?\_ "w")))))
+
 
 (require 'magit)
 ;;  ;; magit stuff!!
